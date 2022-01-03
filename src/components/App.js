aa429c51643e0a052bfb2d3625a947de26c33676
Jeffrey Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-01-03 13:04:32 -0500

    still working on it

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+export default function App(){
+    const[isLoaded, setIsLoaded] = useState(false);
+    const[item, setItems] = useState([]);
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res)=>res.json())
+        .then((data)=>{
+            setIsLoaded(true);
+            setItems(data.message);
+        });
+    })
+
+    if(!isLoaded){
+        return <p>Loading...</p>
+    }else{
+        <div>
+            {item.map(item =>(
+                <image src={item.message}></image>
+            ))}
+        </div>
+    }
+}
